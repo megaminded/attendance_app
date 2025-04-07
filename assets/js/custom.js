15aee8aef612c2df86968bb28e60332cb66c44b4
Genexis-6 <victoryazudoni@gmail.com>
2025-04-07 21:23:03 +0100

    added the warning message to all input section in the registration page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,32 @@
+const validateProfile = (file) =>{
+    const fileType = ["jpg", "jpeg", "png"]
+   return fileType.includes(file.name.split(".")[0])
+}
+
+
 const handleRegistration = (event) =>{
     event.preventDefault()
-    
+    const formObject = {};
     const formData = new FormData(event.target)
-    const firstname = formData.get("firstname")
-    console.log(firstname)
+
+    formData.forEach(
+        (value, key)=>{
+            if(value instanceof File){
+                formObject[key] = value.name
+            }else{
+                if(value.trim() == ""){
+                    document.writeln(
+                        `<div class="alert alert-warning" role="alert">
+                        ${key} filed must not be empty
+                        </div>`
+                    )
+                }else{
+                    formObject[key] = value
+                }
+            }
+        }
+    )
+    console.log(formObject)
+    
+
 }
