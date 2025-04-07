bd1a769b7aaf74f259901b244c0e8d0a4185278c
Genexis-6 <victoryazudoni@gmail.com>
2025-04-07 22:15:30 +0100

    finished with the api call setup for registration

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 const validateProfile = (file) =>{
     const fileType = ["jpg", "jpeg", "png"]
-   return fileType.includes(file.name.split(".")[0])
+   return fileType.includes(file && file.name.split(".")[1].toLowerCase())
 }
 
 
@@ -8,28 +8,42 @@ const handleRegistration = (event) =>{
     event.preventDefault()
     const formObject = {};
     const formData = new FormData(event.target)
+    let formValid = true
 
     clearWarning();
+
     formData.forEach(
         (value, key)=>{
             if(value instanceof File){
-                formObject[key] = value.name
+                if(!validateProfile(value)){
+                        ShowWarning(key, "invalid file type")
+                        formValid = false
+                    }else{
+                        formObject[key] = value
+                    }
+                
+                
             }else{
                 if(value.trim() == ""){
-                    document.writeln(
-                        `<div class="alert alert-warning" role="alert">
-                        ${key} filed must not be empty
-                        </div>`
-                    )
+                    ShowWarning(key, `${key} field must not be empty`)
+                    formValid = false
                 }else{
                     formObject[key] = value
                 }
             }
         }
     )
-    console.log(formObject)
+    formValid && submitRegistrationForm(formObject)
     
+}
 
+
+const ShowWarning = (key, message) =>{
+    const warning = document.querySelector(`#${key}Warning`)
+    if(warning){
+        warning.innerHTML = message;
+        warning.style.display = "block";
+    }
 }
 
 
@@ -37,7 +51,34 @@ const clearWarning = () =>{
     const warningContainer = document.querySelectorAll(".warning-message")
     warningContainer.forEach(
         (warning)=>{
-            warning.style.display = "none"
+            warning.style.display = "none";
         }
     )
+}
+
+
+
+
+
+// communicate with backend api
+
+const submitRegistrationForm = async (payload) =>{
+    try{
+        const fetchData = await fetch("backend_url",{
+            method:"POST",
+            headers:{
+                "Content-type": "application/json",
+            },
+            body:JSON.stringify(payload)
+        })
+        if(fetchData.ok){
+            const data = await fetchData.json()
+        }
+        else{
+            throw new Error("error submitting form")
+        }
+
+    }catch(error){
+        console.log("error due to ", error)
+    }
 }
