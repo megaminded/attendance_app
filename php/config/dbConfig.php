39a2abfd9cfcae97b85663d40273610918a28fb0
unknown <ichukwuebuka26@gmail.com>
2025-04-12 19:45:30 +0100

    untrack dbConfig.php

--- php/config/dbConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DbConfig{
-    //Database configuration
-    protected $hostname = '';   //The hostname
-    protected $username = '';   //The usernamw
-    protected $password = '';   //The password
-    protected $database = '';   //THe name of the database
-}
