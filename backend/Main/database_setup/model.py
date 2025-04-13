0d640f1740753de4a70bfeb2d04e405a9e39ff11
Genexis-6 <victoryazudoni@gmail.com>
2025-04-14 00:19:56 +0100

    added the admin table to the model.py file

--- backend/Main/database_setup/model.py
+++ backend/Main/database_setup/model.py
@@ -13,3 +13,11 @@ class Students(Base):
     email = Column(String(200), nullable=False, index=True)
     profile_pic = Column(String(100), nullable=False, index=True)
     password = Column(String(500))
+
+class Admin(Base):
+    __tablename__ = "admin"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(100), index=True)
+    email = Column(String(200), nullable=False, index=True)
+    password = Column(String(500))
+    
