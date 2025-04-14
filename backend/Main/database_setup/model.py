a802667bd04e0e31b736ee0581c80490281e64ab
Genexis-6 <victoryazudoni@gmail.com>
2025-04-14 03:16:25 +0100

    finined creating the python script to add admin details to the db

--- backend/Main/database_setup/model.py
+++ backend/Main/database_setup/model.py
@@ -1,5 +1,6 @@
 from .database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
+from datetime import datetime
 
 
 class Students(Base):
@@ -13,11 +14,13 @@ class Students(Base):
     email = Column(String(200), nullable=False, index=True)
     profile_pic = Column(String(100), nullable=False, index=True)
     password = Column(String(500))
+    create_at = Column(DateTime, default=datetime.now())
 
 class Admin(Base):
     __tablename__ = "admin"
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     name = Column(String(100), index=True)
-    email = Column(String(200), nullable=False, index=True)
+    email = Column(String(200), nullable=False, index=True, unique=True)
     password = Column(String(500))
+    create_at = Column(DateTime, default=datetime.now())
     
